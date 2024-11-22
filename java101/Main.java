4ea8bc3b9f7143c6e1db75f421a9c92cb79eb23e
kaylee <kayleewu1310@gmail.com>
2024-11-21 21:25:05 -0500

    finished practice problem one

--- java101/Main.java
+++ java101/Main.java
@@ -1,6 +1,20 @@
-public class Main { 
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {};
 
-}
 
-}
+            public static int countOccurences (int[] arr, int n){
+            int sum=0 ;
+            int count=0;
+           for (int x=0 ; x < arr.length; x++ ){
+            if (arr[x]==n){
+                  count++; 
+                }
+           }
+                  return count; 
+            }
+                
+        }
+                 
+                
+                  
+
---
+++ java101/PracticeProblems.java
@@ -0,0 +1,10 @@
+public class PracticeProblems {
+	public static int countOccurences (int[] arr, int n){
+        int sum=0 ;
+        int x=0 ;
+        int count=0;
+       for (int x ; x < arr.length; x++ ){
+        if (arr[x]==n){
+              count++;
+
+}}}}
