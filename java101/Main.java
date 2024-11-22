cd8d2440113fbd684659961d0af8220c0a587070
kaylee <kayleewu1310@gmail.com>
2024-11-21 22:56:31 -0500

    practice problems done

--- java101/Main.java
+++ java101/Main.java
@@ -1,19 +1,5 @@
 public class Main {
     public static void main(String[] args) {};
-
-
-            public static int countOccurences (int[] arr, int n){
-            int sum=0 ;
-            int count=0;
-           for (int x=0 ; x < arr.length; x++ ){
-            if (arr[x]==n){
-                  count++; 
-                }
-           }
-                  return count; 
-            }
-                
-        }
                  
                 
                   
--- java101/PracticeProblems.java
+++ java101/PracticeProblems.java
@@ -1,10 +1,54 @@
 public class PracticeProblems {
 	public static int countOccurences (int[] arr, int n){
-        int sum=0 ;
-        int x=0 ;
-        int count=0;
-       for (int x ; x < arr.length; x++ ){
-        if (arr[x]==n){
-              count++;
+              int count=0;
+             for (int x=0 ; x < arr.length; x++ ){
+              if (arr[x]==n){
+                    count++; 
+                  }
+             }
+                    return count; 
+              }
+                  
+          }
+        
+public class PracticeProblems {
+       public static int reverseArray (int[]arr){
+              int[] reversed = new int[arr.length];
+              for (int x=0; x< arr.length; x++){
+                     reversed[x]= arr[arr.length-1-x];
+              }
+       return reversed;
+       }
+}
 
-}}}}
+public class PracticeProblems {  
+       public static double sumGrid (double[][]grid){
+           double sum= 0;
+       for (int x=0; x<arr.length; x++){
+       for (int y=0; y<arr.length; y++){
+        sum+=grid[x][y];
+       }
+       }
+       return sum;
+}
+}
+
+public class PracticeProblems{
+       public static int fib(int n){
+       int x= 0;
+       int y= 1;
+       for (int p=0; p<=n; p++){
+              fib=x+y;
+              n=fib+y;
+       }
+       return fib;
+}
+}
+
+
+
+
+        
+
+
+
